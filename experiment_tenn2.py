95ed886866336154843d74d9cdf9e2173db998c4
Kevin Zhu <kzhu4@gmu.edu>
2025-02-23 10:32:05 -0500

    switch to aggregation

--- experiment_tenn2.py
+++ experiment_tenn2.py
@@ -73,8 +73,8 @@ class ConnorMillingExperiment(TennExperiment):
             config.spawners[0]['n'] = self.agents
 
         config.metrics = [
+            metrics.Aggregation(history=max(self.cycles, 1)),
             metrics.Circliness(history=max(self.cycles, 1), avg_history_max=450),
-            # metrics.Aggregation(history=max(self.cycles, 1)),
             # metrics.DistanceSizeRatio(history=max(self.cycles, 1)),
         ]
 
