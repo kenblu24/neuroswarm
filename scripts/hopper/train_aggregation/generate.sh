9ee5a59e299eb241f4ae0b59aa31c667f2b04378
Kevin Zhu <kzhu4@gmu.edu>
2025-02-27 10:50:37 -0500

    aggregation for N={3, ..., 10}

--- scripts/hopper/train_aggregation/generate.sh
+++ scripts/hopper/train_aggregation/generate.sh
@@ -6,11 +6,11 @@ set -e
 for i in {3..10}
 do
   # Create a new file for each value
-  new_file="milling-${i}n.slurm"
-  cp milling-0n.slurmtemplate $new_file
+  new_file="aggregation-${i}n.slurm"
+  cp aggregation-0n.slurmtemplate $new_file
 
   # Replace the values in the new file
-  sed -i "s/mill-0n/mill-${i}n/g" $new_file
-  sed -i "s/mill\/0/mill\/${i}/g" $new_file
+  sed -i "s/agg-0n/agg-${i}n/g" $new_file
+  sed -i "s/agg\/0/agg\/${i}/g" $new_file
   sed -i "s/-N 0/-N ${i}/g" $new_file
 done
