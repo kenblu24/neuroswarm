461432e51b074b7749e749832b973ec6a9cee0fd
Kevin Zhu <kzhu4@gmu.edu>
2025-03-29 21:08:55 -0400

    train same seed but vary N inside slurm run

--- scripts/hopper/train_milling/generate.sh
+++ scripts/hopper/train_milling/generate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-for i in {3..10}
+
+# stop on error
+set -e
+
+for i in {25..35}
 do
   # Create a new file for each value
-  new_file="milling-${i}n.slurm"
+  new_file="milling-seed_${i}.slurm"
   cp milling-0n.slurmtemplate $new_file
 
   # Replace the values in the new file
-  sed -i "s/mill-0n/mill-${i}n/g" $new_file
-  sed -i "s/mill\/0/mill\/${i}/g" $new_file
-  sed -i "s/-N 0/-N ${i}/g" $new_file
+  # sed -i "s/mill-0n/mill-${i}n/g" $new_file
+  # sed -i "s/mill\/0/mill\/${i}/g" $new_file
+  sed -i "s/seedint/${i}/g" $new_file
 done
