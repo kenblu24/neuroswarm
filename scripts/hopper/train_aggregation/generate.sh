8082eafd7d99e6a3ed4e64992015477ff615ed52
Kevin Zhu <kzhu4@gmu.edu>
2025-03-29 21:04:42 -0400

    train same seed but vary N inside slurm run

--- scripts/hopper/train_aggregation/generate.sh
+++ scripts/hopper/train_aggregation/generate.sh
@@ -3,14 +3,14 @@
 # stop on error
 set -e
 
-for i in {3..10}
+for i in {25..35}
 do
   # Create a new file for each value
-  new_file="aggregation-${i}n.slurm"
+  new_file="aggregation-seed_${i}.slurm"
   cp aggregation-0n.slurmtemplate $new_file
 
   # Replace the values in the new file
-  sed -i "s/agg-0n/agg-${i}n/g" $new_file
-  sed -i "s/agg\/0/agg\/${i}/g" $new_file
-  sed -i "s/-N 0/-N ${i}/g" $new_file
+  # sed -i "s/agg-0n/agg-${i}n/g" $new_file
+  # sed -i "s/agg\/0/agg\/${i}/g" $new_file
+  sed -i "s/seedint/${i}/g" $new_file
 done
