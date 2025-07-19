c7a3e24dc70c1f231cdd4093ad4826104d6de417
kenblu24 <kenblu24@hotmail.com>
2025-07-19 18:33:44 -0400

    rename to diffusion

--- scripts/hopper/train_diffusion/generate.sh
+++ scripts/hopper/train_diffusion/generate.sh
@@ -6,8 +6,8 @@ set -e
 for i in {25..35}
 do
   # Create a new file for each value
-  new_file="dispersal-seed_${i}.slurm"
-  cp dispersal-0n.slurmtemplate $new_file
+  new_file="diffusion-seed_${i}.slurm"
+  cp diffusion-0n.slurmtemplate $new_file
 
   # Replace the values in the new file
   # sed -i "s/dis-0n/dis-${i}n/g" $new_file
