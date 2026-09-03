04e528a25702a81c8c7a9f3e1dbfa53c76198a26
kenblu24 <kenblu24@hotmail.com>
2026-09-03 16:33:08 -0400

    gui for choosing from given parameters

--- scripts/hopper/slurm/generate.py
+++ scripts/hopper/slurm/generate.py
@@ -1,9 +1,11 @@
+import shutil
 import getpass
 import subprocess
 import pandas as pd
 import pathlib as pl
 from itertools import product
 from swarmsim.util.jinja import make_default_jinja_env
+from wizards import RunParametrizer
 
 
 env = make_default_jinja_env(line_statement_prefix='#%:')
@@ -19,28 +21,27 @@ with open(template_path, 'r') as f:
 
 eons_seeds = [2026, 2027, 2028]
 swarm_sizes = [
-    # 4,
-    # 5,
+    4,
+    5,
     6,
-    # 7,
+    7,
     8,
-    # 9,
+    9,
     10,
-    # 15,
-    # 20,
-    # 25,
-    # 30,
-    # 35,
-    # 40,
-    # 45,
-    # 50,
+    15,
+    20,
+    25,
+    30,
+    35,
+    40,
+    45,
+    50,
 ]
 rngstrats = [
     'TS1',
-    # 'TSG',
-    # 'TSR',
+    'TSG',
+    'TSR',
 ]
-
 shortnames = {
     'Circliness': 'mill',
     'Aggregation': 'aggr',
@@ -48,11 +49,21 @@ shortnames = {
     'DelaunayDiffusion': 'diff',
 }
 behaviors = [
-    # 'Circliness',
+    'Circliness',
     'Aggregation',
     'ExplodingDispersion',
     'DelaunayDiffusion',
 ]
+
+# ask the user to choose parameters
+behaviors, swarm_sizes, eons_seeds, rngstrats = RunParametrizer({
+    'Behavior': behaviors,
+    'Number of Agents': swarm_sizes,
+    'EONS Seed': eons_seeds,
+    'RNG Strategy': rngstrats,
+}).run().values()
+# TODO: persisting old parameter selections to file
+# TODO: pasting parameter lists from clipboard. need to check types.
 
 configs = []
 for behavior, swarm_size, eons_seed, rngstrat in product(
@@ -71,14 +82,37 @@ for behavior, swarm_size, eons_seed, rngstrat in product(
         rngstrat=rngstrat,
     ))
 
+# TODO: textual app to examine generated slurm files, modify slurm parameters like time
+# TODO: textual interface check for and delete old runs, inspect .err, etc.
+
 print(pd.DataFrame(configs))
 input('Press enter to continue or ctrl-c to cancel.')
 
+existing_runs = [path for cfg in configs for path in [cfg['projpath']]
+                 if path.is_dir() and path.glob('*.err')]
+
+if existing_runs:
+    print(f"WARNING: Detected existing logs from previous runs for:")
+    for p in existing_runs:
+        print(p)
+    print("\nType 'REMOVE' to delete them, or 'CONTINUE' to continue anyway.")
+    response = input(': ')
+    if response.strip() == 'REMOVE':
+        for p in existing_runs:
+            print(f"Removing {p}")
+            shutil.rmtree(p)
+        print("Done.")
+    elif response.strip() == 'CONTINUE':
+        pass
+    else:
+        print("Invalid response received. Aborting.")
+        exit(-1)
+
 for d in configs:
-    slurm = template.render(**d)
     projpath = d['projpath']
-    print(projpath)
     projpath.mkdir(parents=True, exist_ok=True)
-    with open(f"{d['projpath']}/sbatch.slurm", 'w') as f:
+    slurm = template.render(**d)
+    print(str(projpath))
+    with open(f"{projpath}/sbatch.slurm", 'w') as f:
         f.write(slurm)
-    subprocess.run(f"sbatch {d['projpath']}/sbatch.slurm", shell=True)
+    subprocess.run(f"sbatch {projpath}/sbatch.slurm", shell=True)
