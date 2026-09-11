6769267769db91b3b86010d66d62ea61f2cd1ce8
kenblu24 <kenblu24@hotmail.com>
2026-09-10 20:08:04 -0400

    tie resolution for caspyan as well, rename argument

--- rss/CaspianBinaryRemappedController.py
+++ rss/CaspianBinaryRemappedController.py
@@ -7,9 +7,9 @@ from .CaspianBinaryController import CaspianBinaryController
 
 
 class CaspianBinaryRemappedController(CaspianBinaryController):
-    def __init__(self, cont_mode: int, *args, **kwargs):
+    def __init__(self, *args, resolve_ties=1, **kwargs):
         super().__init__(*args, **kwargs)
-        self.cont_mode = cont_mode
+        self.resolve_ties = resolve_ties
 
     @override
     def run_processor(self, observation):
@@ -41,51 +41,44 @@ class CaspianBinaryRemappedController(CaspianBinaryController):
         v = v_mapping[data[1]] - v_mapping[data[0]]
         w = w_mapping[data[3]] - w_mapping[data[2]]
 
-        match self.cont_mode:
-            case 0:
-                # Do nothing
-                pass
-
-            case 1:
-                # Use RNG to split ties
-                if v == 0.0:
-                    v = v_mapping[1] * self.parent.rng.choice([-1, 1])
-                if w == 0.0:
-                    w = w_mapping[1] * self.parent.rng.choice([-1, 1])
-
-            case 2:
-                # Force v to -1, w to -1
-                if v == 0.0:
-                    v = -v_mapping[1]
-                if w == 0.0:
-                    w = -w_mapping[1]
-
-            case 3:
-                # Force v to -1, w to 1
-                if v == 0.0:
-                    v = -v_mapping[1]
-                if w == 0.0:
-                    w = w_mapping[1]
-
-            case 4:
-                # Force v to 1, w to -1
-                if v == 0.0:
-                    v = v_mapping[1]
-                if w == 0.0:
-                    w = -w_mapping[1]
-
-            case 5:
-                # Force v to 1, w to 1
-                if v == 0.0:
-                    v = v_mapping[1]
-                if w == 0.0:
-                    w = w_mapping[1]
-
-            case 6:
-                if v == 0.0 or w == 0.0:
-                    v, w = 0.0, 0.0
-
-            case _: raise ValueError("cont mode should be in [0, 7)")
+        if self.resolve_ties == 0:
+            # Do nothing
+            pass
+        elif self.resolve_ties == 1:
+            # Use RNG to split ties
+            if v == 0.0:
+                v = v_mapping[1] * self.parent.rng.choice([-1, 1])
+            if w == 0.0:
+                w = w_mapping[1] * self.parent.rng.choice([-1, 1])
+        elif self.resolve_ties == 2:
+            # Force v to -1, w to -1
+            if v == 0.0:
+                v = -v_mapping[1]
+            if w == 0.0:
+                w = -w_mapping[1]
+        elif self.resolve_ties == 3:
+            # Force v to -1, w to 1
+            if v == 0.0:
+                v = -v_mapping[1]
+            if w == 0.0:
+                w = w_mapping[1]
+        elif self.resolve_ties == 4:
+            # Force v to 1, w to -1
+            if v == 0.0:
+                v = v_mapping[1]
+            if w == 0.0:
+                w = -w_mapping[1]
+        elif self.resolve_ties == 5:
+            # Force v to 1, w to 1
+            if v == 0.0:
+                v = v_mapping[1]
+            if w == 0.0:
+                w = w_mapping[1]
+        elif self.resolve_ties == 6:
+            if v == 0.0 or w == 0.0:
+                v, w = 0.0, 0.0
+        else:
+            raise ValueError("resolve_ties should be a choice from 0-6")
 
         return v, w
         # return (0.08, 0.4) if not observation else (0.18, 0.0)  # CMA best controller
